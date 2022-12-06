c4ca31e7a51bf7a8594cd31052158f34ef015be2
Austin-r-King <de.king.hi@gmail.com>
2022-12-06 11:58:26 -0500

    intinal state

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import yfinance as yf
+import datetime
 
 
-ticker = yf.Ticker('KO').info
-market_price = ticker['regularMarketPrice']
-previous_close_price = ticker['regularMarketPreviousClose']
-print('Ticker: KO')
-print('Market Price:', market_price)
-print('Previous Close Price:', previous_close_price)
+today = datetime.datetime.now()
+d = datetime.timedelta(days = 50)# amount of days subtracted from today
+a = today - d
+
+
 
 #WEFGQEUIBVQEUB3B#
+
+def KO_Info():
+    ticker = yf.Ticker('KO').info
+    market_price = ticker['regularMarketPrice']
+    previous_close_price = ticker['regularMarketPreviousClose']
+    print('Ticker: KO')
+    print('Market Price:', market_price)
+    print('Previous Close Price:', previous_close_price)
+
+
+
