5697f7b1446b1e2819764f6120542348e9bce95c
Austin-r-King <de.king.hi@gmail.com>
2022-12-08 10:26:14 -0500

    intinal state

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import yfinance as yf
+import numpy as np
+from matplotlib import animation as animation, pyplot as plt, cm
 import datetime
-
-
-today = datetime.datetime.now()
-d = datetime.timedelta(days = 50)# amount of days subtracted from today
-a = today - d
+from datetime import date
 
 
 
@@ -17,6 +15,28 @@ def KO_Info():
     print('Ticker: KO')
     print('Market Price:', market_price)
     print('Previous Close Price:', previous_close_price)
+
+
+def getting_data_week():
+    today = date.today()
+    PastWeek = today - date.today(days=7)
+    start_date = PastWeek
+    end_date = today
+    ko= yf.Ticker("KO")
+    ko_historical = ko.history(start= PastWeek, end="today", interval="1m")
+    print(ko_historical)
+#getting_data_week()
+
+today = datetime.datetime.today()
+Time = str(today - datetime.timedelta(days=7))
+i=0
+PastWeek = ""
+while (i <11):
+    PastWeek = PastWeek + Time[i]
+    i += 1
+
+
+print(PastWeek)
 
 
 
