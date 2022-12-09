06a99ddfaa0dd2340263e10aa4089826e48b6fa2
Austin-r-King <de.king.hi@gmail.com>
2022-12-09 12:01:31 -0500

    intinal state

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import numpy as np
 from matplotlib import animation as animation, pyplot as plt, cm
 import datetime
 from datetime import date
+import seaborn
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
-
-#WEFGQEUIBVQEUB3B#
 
 def KO_Info():
     ticker = yf.Ticker('KO').info
@@ -18,25 +20,26 @@ def KO_Info():
 
 
 def getting_data_week():
+    today = datetime.datetime.today()
+    Time = str(today - datetime.timedelta(days=7))
+    i = 0
+    pastWeek = ""
+    while (i < 11):
+        pastWeek = pastWeek + Time[i]
+        i += 1
     today = date.today()
-    PastWeek = today - date.today(days=7)
-    start_date = PastWeek
+    start_date = pastWeek
     end_date = today
     ko= yf.Ticker("KO")
-    ko_historical = ko.history(start= PastWeek, end="today", interval="1m")
+    ko_historical = ko.history(start= pastWeek, end=today, interval="1m")
     print(ko_historical)
 #getting_data_week()
 
-today = datetime.datetime.today()
-Time = str(today - datetime.timedelta(days=7))
-i=0
-PastWeek = ""
-while (i <11):
-    PastWeek = PastWeek + Time[i]
-    i += 1
+aapl= yf.Ticker("aapl")
+print(aapl)
+aapl_historical = aapl.history(start="2020-06-02", end="2020-06-07", interval="1m")
+aapl_historical
 
-
-print(PastWeek)
 
 
 
