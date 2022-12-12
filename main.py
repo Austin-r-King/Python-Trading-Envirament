13033452f416fdea2d7a041530add8065365d8ed
Austin-r-King <de.king.hi@gmail.com>
2022-12-12 10:48:15 -0500

    intinal state

--- main.py
+++ main.py
@@ -14,32 +14,15 @@ def KO_Info():
     ticker = yf.Ticker('KO').info
     market_price = ticker['regularMarketPrice']
     previous_close_price = ticker['regularMarketPreviousClose']
-    print('Ticker: KO')
+#    print('Ticker: KO')
     print('Market Price:', market_price)
-    print('Previous Close Price:', previous_close_price)
+#    print('Previous Close Price:', previous_close_price)
 
-
-def getting_data_week():
-    today = datetime.datetime.today()
-    Time = str(today - datetime.timedelta(days=7))
-    i = 0
-    pastWeek = ""
-    while (i < 11):
-        pastWeek = pastWeek + Time[i]
-        i += 1
+print(datetime.datetime.today())
+def getting_data_today():
     today = date.today()
-    start_date = pastWeek
-    end_date = today
-    ko= yf.Ticker("KO")
-    ko_historical = ko.history(start= pastWeek, end=today, interval="1m")
-    print(ko_historical)
-#getting_data_week()
+    ticker = yf.Ticker('KO').info
+    print (market_price = ticker['regularMarketPrice'])
+    while (datetime.time.today < today+"10:23:28"):
 
-aapl= yf.Ticker("aapl")
-print(aapl)
-aapl_historical = aapl.history(start="2020-06-02", end="2020-06-07", interval="1m")
-aapl_historical
-
-
-
-
+#KO_Info()
