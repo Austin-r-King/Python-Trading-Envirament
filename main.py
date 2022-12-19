1457de0b7a2839eb2dd23e3bff9d384e0d36e4c3
Austin-r-King <de.king.hi@gmail.com>
2022-12-19 12:02:22 -0500

    intinal state

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 import yfinance as yf
-import numpy as np
-from matplotlib import animation as animation, pyplot as plt, cm
 import datetime
 from datetime import date
-import seaborn
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
 import time
 
 def KO_Info():
@@ -17,15 +11,18 @@ def KO_Info():
     print('Market Price:', market_price)
 #    print('Previous Close Price:', previous_close_price)
 
-print(datetime.datetime.today())
+
+
 def getting_data_today():
+    f = ('KO prices and dates.txt', 'w')
     today = date.today()
     open = datetime.datetime.today().replace(hour=9, minute=3, second=0, microsecond=0)
     ticker = yf.Ticker('KO').info
     market_price = ticker['regularMarketPrice']
     if datetime.datetime.today() >= open:
         for amount_saved in range (0,78):
-            print(market_price)
-            print(datetime.datetime.today().replace( microsecond=0 ) )
-            time.sleep(300)
+            f = open("KO prices and dates.txt", "a")
+            time = "lol"
+            f.write(time + market_price)
+            f.close()
 getting_data_today()
