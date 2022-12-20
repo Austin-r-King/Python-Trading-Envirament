3567f34d4d244094a068dd304bc4698e4c45200e
Austin-r-King <de.king.hi@gmail.com>
2022-12-20 10:15:05 -0500

    intinal state

--- main.py
+++ main.py
@@ -1,28 +1,24 @@
 import yfinance as yf
 import datetime
 from datetime import date
-import time
 
 def KO_Info():
     ticker = yf.Ticker('KO').info
     market_price = ticker['regularMarketPrice']
-    previous_close_price = ticker['regularMarketPreviousClose']
-#    print('Ticker: KO')
+    print('Ticker: KO')
     print('Market Price:', market_price)
-#    print('Previous Close Price:', previous_close_price)
 
 
 
 def getting_data_today():
-    f = ('KO prices and dates.txt', 'w')
     today = date.today()
-    open = datetime.datetime.today().replace(hour=9, minute=3, second=0, microsecond=0)
+    OpenTime = datetime.datetime.today().replace(hour=9, minute=3, second=0, microsecond=0)
     ticker = yf.Ticker('KO').info
-    market_price = ticker['regularMarketPrice']
-    if datetime.datetime.today() >= open:
+    if datetime.datetime.today() >= OpenTime:
         for amount_saved in range (0,78):
+            market_price = ticker['regularMarketPrice']
             f = open("KO prices and dates.txt", "a")
-            time = "lol"
-            f.write(time + market_price)
+            time = datetime.datetime.today().replace(second=0, microsecond=0)
+            f.write(str(market_price)+":"+str(time)+" - ")
             f.close()
 getting_data_today()
